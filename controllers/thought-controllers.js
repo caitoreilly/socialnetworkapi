ceb408aab08c190f2299692212b73b8b68c8c544
Caitlin O'Reilly <coreilly29@gmail.com>
2022-12-11 15:24:46 -0500

    added notes for which requests to create in thought-controllers.js & user-controllers.js

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -1,7 +1,23 @@
-const {Thought} = require('../models');
+const {Thought, User} = require('../models');
 
 const thoughtController = {
     // functions will go here as methods 
+    // /api/thoughts
+
+    // GET all thoughts
+
+    // GET a single thought by its _id
+
+    // POST to create a new thought (push the created thought's _id to the associated user's thoughts array field)
+
+    // PUT to update a thought by its _id
+
+    // DELETE to remove a thought by its _id
+
+    // /api/thoughts/:thoughtId/reactions
+    // POST to create a reaction stored in a single thought's reactions array field
+    
+    // DELETE to pull & remove a reaction by the reaction's reactionId value 
 }
 
 module.exports = thoughtController;
--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,7 +1,23 @@
-const {User} = require("../models");
+const {User, Thought} = require("../models");
 
 const userController = {
-    // functions will go here as methods 
+  // functions will go here as methods
+  // /api/users
+
+  // GET all users
+
+  // GET a single user by its _id and populated thought & friend data
+  
+  // POST a new user (create a new user)
+  
+  // PUT to update a user by its _id
+  
+  // DELETE to remove user by its _id
+
+  // /api/users/:userId/friends/:friendId
+  // POST to Add a new friend to a user's friend list
+
+  // DELETE to remove a friend from a user's friend list 
 };
 
 module.exports = userController;
